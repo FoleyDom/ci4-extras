05509d1e3587dfb17585c77b2e3d55f53771b553
Dom Foley <dfoleydev@gmail.com>
2023-07-01 14:03:53 -0400

    moods controller test

--- app/Controllers/MoodsController.php
+++ app/Controllers/MoodsController.php
@@ -10,6 +10,15 @@ class MoodsController extends BaseController
 {
     public function index()
     {
-        //
+        $model = new MoodsModel();
+
+        $data = [
+            'title' => 'Home Page',
+            'tab_title' => 'Home Page'
+        ];
+
+        echo view('templates/global_header', $data);
+        echo view('moods/index', $data);
+        echo view('templates/global_footer');
     }
 }
